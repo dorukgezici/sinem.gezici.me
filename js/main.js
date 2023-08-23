b8af3d1c02c924e4bb350f46db7acfb95b019a49
Doruk Gezici <doruk@gezici.me>
2023-08-23 18:05:02 +0200

    Deploy to GitHub Pages

--- js/main.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js').then(r => {
-        console.log('Service Worker registered');
-    }).catch(e => {
-        console.log('Service Worker registration failed: ', e);
-    });
-}
